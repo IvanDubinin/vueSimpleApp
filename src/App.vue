3927ffad6b880cff805f50975e6d180ea1d46d74
Ivan <Apollonkrit2@gmail.com>
2021-03-25 15:44:28 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app" class="add">
+    <button v-on:click="incrementNumber">⬆︎</button>
+    <input v-model="number" />
+    <Calculate :data="Array.from({ length: number }, (v, i) => 10 + i)" />
+  </div>
+</template>
+
+<script>
+import Calculate from "./components/Calculate.vue";
+
+export default {
+  name: "App",
+  components: {
+    Calculate,
+  },
+  data() {
+    return {
+      number: 5,
+    };
+  },
+  methods: {
+    incrementNumber() {
+      this.number++;
+    },
+  },
+};
+</script>
+
+<style>
+div {
+  box-sizing: border-box;
+  color: rgb(36, 41, 46);
+  display: block;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: 16px;
+  height: 600px;
+  line-height: 24px;
+  margin-bottom: 32px;
+  margin-left: 340px;
+  margin-right: 340px;
+  margin-top: 32px;
+  max-width: 1012px;
+  padding-left: 16px;
+  padding-right: 16px;
+  width: 1012px;
+}
+button {
+  background-color: rgb(192, 192, 192);
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  height: 35px;
+  width: 35px;
+  margin-left: 350px;
+  margin-right: 10px;
+}
+input {
+  background-color: rgb(255, 255, 255);
+  border-width: 2px 2px;
+  color: rgb(0, 0, 0);
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: 16px;
+  height: 29px;
+  width: 200px;
+}
+</style>
+
---
+++ src/components/Calculate.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in data" :key="data[item]">
+        <h1>
+          <span>2</span><sup>{{ item }}</sup> = {{ 2 ** item }}
+        </h1>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculate",
+  props: {
+    data: [Array],
+  },
+};
+</script>
+
+
+<style>
+ul {
+  box-sizing: border-box;
+  color: rgb(36, 41, 46);
+  display: block;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: 16px;
+  height: 420px;
+  line-height: 24px;
+  list-style-type: disc;
+  margin-block-end: 16px;
+  margin-bottom: 16px;
+  margin-top: 0px;
+  padding-inline-start: 32px;
+  padding-left: 32px;
+  width: 980px;
+}
+li {
+  box-sizing: border-box;
+  color: rgb(36, 41, 46);
+  display: list-item;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: 16px;
+  height: 50px;
+  line-height: 24px;
+  list-style-type: disc;
+  margin-top: 4px;
+  text-align: left;
+  width: 948px;
+}
+</style>
